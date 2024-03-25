bd7d9b7ba9233fe804d9fe23590233f110271f05
Diego Gavancho <dg.gavancho@gmail.com>
2024-03-25 15:47:29 -0500

    Correccion actualizacion de carga

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ async def processing():
 
     try:
             # chatgpt_responses = await asyncio.gather(*[track_and_execute(index, task, counter_semaphore) for index, task in enumerate(tasks)])
-        tasks_coroutines = [asyncio.wait_for(track_and_execute(index, task, counter_semaphore), 3) for index, task in enumerate(tasks)]
+        tasks_coroutines = [asyncio.wait_for(track_and_execute(index, task, counter_semaphore), 12) for index, task in enumerate(tasks)]
         chatgpt_responses = await asyncio.gather(*tasks_coroutines) 
         print('Tiempo suficiente')
     except Exception as e:
